9fcdd243d285568afae66cbff9e91c572b3cbb26
hyu22 <hyu22@ncsu.edu>
2020-10-08 22:54:40 -0400

    complete methods in get_bs.c and release_bs.c

--- paging/get_bs.c
+++ paging/get_bs.c
@@ -5,7 +5,35 @@
 
 int get_bs(bsd_t bs_id, unsigned int npages) {
     /* requests a new mapping of npages with ID map_id */
-    
+    STATWORD ps;
+    disable(ps);
+
+    if (bs_id < 0 || bs_id >= MAX_NUM_BS) {
+        restore(ps);
+        return SYSERR;
+    }
+
+    if (npages <= 0 || npages > MAX_BS_PAGES) {
+        restore(ps);
+        return SYSERR;
+    }
+
+    int pid = getpid;
+
+    if (bsm_tab[bs_id].bs_status == BSM_MAPPED) {
+        if (bsm_tab[bs_id].bs_pid != pid) {
+            restore(ps);
+            return SYSERR;
+        }
+        npages = bsm_tab[bs_id].bs_npages;
+    } else {
+        bsm_tab[bs_id].bs_status = BSM_MAPPED;
+        bsm_tab[bs_id].bs_pid = pid;
+        bsm_tab[bs_id].bs_npages = npages;
+    }
+
+    restore(ps);
+    return npages;
 }
 
 
--- paging/release_bs.c
+++ paging/release_bs.c
@@ -4,10 +4,26 @@
 #include <paging.h>
 
 SYSCALL release_bs(bsd_t bs_id) {
+    /* release the backing store with ID bs_id */
+    STATWORD ps;
+    disable(ps);
 
-  /* release the backing store with ID bs_id */
-    kprintf("To be implemented!\n");
-   return OK;
+    // bs_id is not in valid range
+    if (bs_id < 0 || bs_id >= MAX_NUM_BS) {
+        restore(ps);
+        return SYSERR;
+    }
 
+    int pid = getpid();
+
+    // pid of the current process != pid of the backing store
+    if (pid != bsm_tab[bs_id].bs_pid) {
+        restore(ps);
+        return SYSERR;
+    }
+
+    restore(ps);
+
+    return free_bsm(bs_id);
 }
 
